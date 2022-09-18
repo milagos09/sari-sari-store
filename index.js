81795c7d2c80f54537f1933aee2c3f73220d2c51
unknown <milagos31@gmail.com>
2022-09-18 12:22:05 +0800

    initial

---
+++ index.js
@@ -0,0 +1,63 @@
+function countPages(totalRows = mock.length, rowsPerPage = 5) {
+    return Math.ceil(totalRows / rowsPerPage);
+}
+
+function getPageArray(page = 0, rowCount = 5) {
+    const currentPage = mock.slice(page * rowCount, rowCount + page * rowCount);
+    return currentPage;
+}
+
+function createPages(pageCount = countPages(), active = 0) {
+    const footerRow = document.getElementById("pagination");
+    for (let i = 0; i < pageCount; i++) {
+        const pageButton = document.createElement("button");
+        pageButton.innerText = i + 1;
+        pageButton.value = i;
+        pageButton.classList = i === active ? "btn-page active" : "btn-page";
+        pageButton.disabled = active === i;
+        footerRow.appendChild(pageButton);
+    }
+
+    const pageButtons = document.querySelectorAll(".btn-page");
+    for (const btn of pageButtons) {
+        btn.addEventListener("click", (e) => {
+            const newActive = e.target.value;
+            const currentItems = getPageArray(newActive);
+            fillTable(currentItems);
+            for (const btn2 of pageButtons) {
+                if (btn2.value == newActive) {
+                    btn2.className = "btn-page active";
+                    btn2.disabled = true;
+                } else {
+                    btn2.className = "btn-page";
+                    btn2.disabled = false;
+                }
+            }
+        });
+    }
+}
+
+function fillTable(items = getPageArray(), rows = 5) {
+    const itemsTable = document.getElementById("items-table");
+    itemsTable.innerHTML = "";
+    items.forEach((e, i) => {
+        const tr = document.createElement("tr");
+        const tdItem = document.createElement("td");
+        const tdPrice = document.createElement("td");
+
+        tdItem.textContent = e.item;
+        tdPrice.textContent = "P" + e.price.toFixed(2);
+
+        tr.append(tdItem, tdPrice);
+        itemsTable.append(tr);
+    });
+}
+
+function paginate() {}
+
+//Event Listeners
+document.getElementById("search").addEventListener("keyup", (e) => {});
+
+console.log(countPages());
+fillTable();
+createPages();
